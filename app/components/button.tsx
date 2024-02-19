2f4c14c98a8c4d784d248b1898967bfe166f4345
Wilbur Hu <wilburhu83@gmail.com>
2024-02-19 09:46:11 +0800

    Update

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-export default function Button({ children, className }: { children?: React.ReactNode, className?: string}) {
-
-    return (
-        <button className={`bg-grey text-white p-2 rounded-md self-stretch hover:bg-green hover:text-off-black ${className}`}>
-            {children}
-        </button>
-    )
+export default function Button({
+  children,
+  className,
+}: {
+  children?: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <button
+      className={`bg-grey text-sm text-white p-2 rounded-md self-stretch h-11 hover:bg-green hover:text-off-black ${className}`}
+    >
+      {children}
+    </button>
+  );
 }
